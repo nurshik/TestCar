d24e36d7bd838afd408d43b33d71e27890b7be06
nurshik <nurshik1@gmail.com>
2021-06-12 13:42:04 -0500

    check

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,10 @@ public class Main {
         audi.number="117";
         audi.door="3";
         audi.model="R8";
+
         test case for immediate requirement
+
+        test cases to test model of the car
+        added some important test cases and he'll be done very soon
     }
 }
